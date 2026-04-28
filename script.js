369bf3916164d89201aca5426d3dfb6ff14bd611
Juanluisdiaz <Luisdiaz732@hotmail.com>
2026-04-27 20:50:31 -0400

    nuevo rediseño de acerinox actualizado

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
-document.getElementById("formularioContacto").addEventListener("submit", function(event) {
-  event.preventDefault();
-  alert("¡Gracias por contactarnos! Te responderemos pronto.");
-});
-
-let slideIndex = 0;
-const slides = document.querySelectorAll('.slide');
-
-function showSlides() {
-  // Oculta todas las imágenes
-  slides.forEach(slide => slide.classList.remove('active'));
-
-  // Muestra la siguiente imagen
-  slideIndex = (slideIndex + 1) % slides.length;
-  slides[slideIndex].classList.add('active');
-}
-
-// Mostrar la primera imagen al cargar
-slides[slideIndex].classList.add('active');
-
-// Cambiar cada 3 segundos
-setInterval(showSlides, 3000); // Puedes cambiar el tiempo si deseas
-
-function mostrarCategoria(categoriaId) {
+function mostrarCategoria(id) {
     const categorias = document.querySelectorAll('.categoria');
 
     categorias.forEach(cat => {
         cat.style.display = 'none';
     });
 
-    document.getElementById(categoriaId).style.display = 'block';
+    document.getElementById(id).style.display = 'block';
 }
+
+// Mostrar una por defecto
+window.onload = function() {
+    mostrarCategoria('escaleras');
+};
